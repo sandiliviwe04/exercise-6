d9488a16e1fa2df9af163d9890aa04f40c73997a
Liviwe Sandi <sandi.liviwe04@gmail.com>
2025-11-03 14:23:12 +0200

    Initial  EX6

---
+++ functions.js
@@ -0,0 +1,13 @@
+function greet(name) {
+    return `Hello ${name}!`;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+let greeting = greet("Ty");
+let sum = add(10, 6);
+
+console.log(greeting);
+console.log(sum);
